8278b8eb6a778455917535877b43d6606a304eb6
dell <jainulabdincse@gmail.com>
2023-05-13 01:23:20 +0530

    any

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String args[])
     {
         RWOnly first=new RWOnly();
-        first.setName("hi");// seeting name
-        System.out.println("silu");
+        first.setName("NAME");// seeting name
+        System.out.println(first.getName());
     }
 
   
