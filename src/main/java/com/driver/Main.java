eb78f8eec1512f17bfa25b27d68d9ad615771d60
shaik jainulabdin <jainulabdincse@gmail.com>
2023-05-12 21:33:15 +0530

    completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String args[])
+    {
+        RWOnly first=new RWOnly();
+        first.setName("hi");
+        System.out.println(first.getName());
+    }
+
   
 }
