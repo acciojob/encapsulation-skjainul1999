2f7a6c2170077fa73b863098e9e488d5a3e6c6bd
dell <jainulabdincse@gmail.com>
2023-05-12 22:34:04 +0530

    acci

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String args[])
     {
         RWOnly first=new RWOnly();
-        first.setName("hi");
+        first.setName("hi");// seeting name
         System.out.println(first.getName());
     }
 
